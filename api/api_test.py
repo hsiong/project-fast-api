c0bdf0371eb40e91d4538cbad6882ad578a19152
hsiong <peak1203@126.com>
2026-05-17 18:14:47 +0800

    Closes:(#6)Refactor: apply standard code formatting

--- api/api_test.py
+++ api/api_test.py
@@ -6,6 +6,9 @@ from util.bean_util import dict_to_bean
 
 router = APIRouter()
 
+
 @router.get("test")
 async def render_page(profile_id: str, page_type: str, request: Request):
-	return Result.ok(data=dict_to_bean({"profile_id": profile_id, "page_type": page_type}))
+    return Result.ok(
+        data=dict_to_bean({"profile_id": profile_id, "page_type": page_type})
+    )
