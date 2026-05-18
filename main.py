d6a2f0024a3a9650b4f23a40f84e785aee30d32f
hsiong <peak1203@126.com>
2026-05-18 18:22:31 +0800

    Closes:(#10)feature: add postgresql session initialization

--- main.py
+++ main.py
@@ -6,36 +6,46 @@ from fastapi import FastAPI
 from api.api_test import router as test_router
 from config.settings import SERVICE_PORT
 from core.log import setup_logging
+from core.postgres_init import init_postgres
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(title="Aesthetic Customer Value Engine")
+	app = FastAPI(title="Aesthetic Customer Value Engine")
+	
+	app.include_router(test_router, prefix="/api", tags=["attendance"])
+	# app.include_router(test_router, prefix="/api", tags=["test"])
+	
+	return app
 
-    app.include_router(test_router, prefix="/api", tags=["test"])
 
-    return app
+def init(app: FastAPI) -> None:
+	setup_logging()  # 初始化日志
+	init_postgres(app)
+	
+	# from model.repo.attendance_statistics_repo import AttendanceStatisticsRepo
+	# from model.service.attendance_statistics_service import AttendanceStatisticsService
+	# app.attendanceStatisticsRepo = AttendanceStatisticsRepo(app.postgresSession)
+	# app.attendanceStatisticsService = AttendanceStatisticsService(
+	# 	app.attendanceStatisticsRepo
+	# )
 
 
 app = create_app()
-setup_logging()  # 初始化日志
-
+init(app)
 
 if __name__ == "__main__":
-    # uvicorn.run(
-    # 	"main:app",
-    # 	host="0.0.0.0",
-    # 	port=8001, # lsof -i: $port|awk '{if(NR>=2) print $2}'|xargs kill
-    # 	# reload=True, # 目录里的文件有改动,就自动重启服务。
-    # 	# log_level="debug",
-    # )
-    config = uvicorn.Config(
-        "main:app",
-        host="0.0.0.0",
-        port=SERVICE_PORT,
-        log_config=None,  # 🔥 关键：禁止 uvicorn 覆盖 logging
-        access_log=True,  # 可选：确保 access log 打开
-    )
-    server = uvicorn.Server(config)
-    import asyncio
-
-    asyncio.get_event_loop().run_until_complete(server.serve())
+	# uvicorn.run(
+	# 	"main:app",
+	# 	host="0.0.0.0",
+	# 	port=8001, # lsof -i: $port|awk '{if(NR>=2) print $2}'|xargs kill
+	# 	# reload=True, # 目录里的文件有改动,就自动重启服务。
+	# 	# log_level="debug",
+	# )
+	config = uvicorn.Config(
+		"main:app", host="0.0.0.0", port=SERVICE_PORT, log_config=None,  # 🔥 关键：禁止 uvicorn 覆盖 logging
+		access_log=True,  # 可选：确保 access log 打开
+	)
+	server = uvicorn.Server(config)
+	import asyncio
+	
+	asyncio.get_event_loop().run_until_complete(server.serve())
--- requirements.t
+++ requirements.t
@@ -1,6 +1,11 @@
+-i https://pypi.tuna.tsinghua.edu.cn/simple
+
 fastapi==0.121.2
 uvicorn==0.38.0
 requests==2.31.0
 numpy==2.3.5
 pydantic==2.12.4
 python-dotenv==1.2.1
+
+# database
+SQLAlchemy==2.0.44
